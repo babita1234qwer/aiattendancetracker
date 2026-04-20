f9e40b577224d0f5138ed1dd12816a21761e1373
ANKUR982U <patelsebu2006@gmail.com>
2026-04-20 18:46:55 +0530

    first commit

---
+++ src/screens/teacherscreen.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from src.ui.base_layout import style_base_layout, style_background_dashboard
+from src.components.header import header_dashboard
+
+from src.components.footer import footer_dashboard
+def teacher_screen():
+    style_background_dashboard()
+    style_base_layout()
+    if "teacher_login_type" not in st.session_state:
+        st.session_state.teacher_login_type = "login"
+
+    if st.session_state.teacher_login_type == "login":
+        teacher_screen_login()
+    elif st.session_state.teacher_login_type == "register":
+        teacher_screen__register()    
+
+    
+
+
+def teacher_screen_login():
+   c1,c2=st.columns(2,vertical_alignment="center",gap="xxlarge")
+   with c1:
+      header_dashboard()
+   with c2:
+    if st.button("Go back to Home", type="secondary", key="loginbackbtn"):
+     st.session_state["login_type"] = None
+     st.rerun()
+   st.header('Login using password',text_alignment="center")
+   st.space()
+   st.space()
+   teacher_username=st.text_input("Enter username",placeholder="ananyaroy")
+   teacher_pass=st.text_input("Enter password",type="password",placeholder="Enter password")
+   st.divider()
+   btnc1,btnc2=st.columns(2)
+
+   with btnc1:
+       st.button('Login',icon=':material/passkey:',shortcut="control+enter",width='stretch')
+
+   with btnc2:
+       if st.button("Register Instead",type="primary",icon=":material/passkey:",width="stretch"):
+              st.session_state.teacher_login_type="register"
+              
+         
+       
+
+   footer_dashboard()
+def teacher_screen__register():
+    c1,c2=st.columns(2,vertical_alignment="center",gap="xxlarge")
+    with c1:
+        header_dashboard()
+    with c2:
+         if st.button("Go back to Home",type="secondary",key="loginbackbtn",shortcut="control+backspace"):
+             st.session_state["login_type"] = None
+             st.rerun()
+
+    st.header("Register your teacher profile")
+    st.space()
+    st.space()
+    teacher_username=st.text_input("Enter username",placeholder="ananyaroy")
+    teacher_name=st.text_input("Enter name",placeholder="Ananya Roy")
+    teacher_pass=st.text_input("Enter password",type="password",placeholder="Enter password")
+    teacher_pass_confirm=st.text_input("Confirm your password",type="password",placeholder="Enter password")
+    
+    st.divider()
+    btnc1,btnc2=st.columns(2)
+
+    with btnc1:
+       st.button('Register',icon=':material/passkey:',shortcut="control+enter",width='stretch')
+
+    with btnc2:
+       if st.button("Login Instead",type="primary",icon=":material/passkey:",width="stretch"):
+                st.session_state.teacher_login_type="login"
+
+    footer_dashboard()
+
+
+
+   
+    
---
+++ src/ui/base_layout.py
@@ -0,0 +1,118 @@
+import streamlit as st
+
+
+def style_background_home():
+
+    st.markdown("""
+                <style>
+                
+                .stApp {
+                background:#5B65F2   !important;}
+
+                .stApp div[data-testid="stColumn"]{
+                  background-color:#E0E3FF !important;
+                  color:black !important;
+                  padding:1.5rem !important;
+                  border-radius:5rem !important;}
+                </style>
+                """, unsafe_allow_html=True)
+    
+def style_background_dashboard():
+    st.markdown("""
+                <style>
+                    .stApp {
+                background:#E0E3FF  !important;}
+                </style>
+                """, unsafe_allow_html=True
+                )
+
+
+
+
+
+
+
+
+def style_base_layout():
+    # asdasd
+    st.markdown("""
+                <style>
+                
+                @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
+                @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
+
+                /* Hide Top Bar  of streamlit*/
+
+
+                 MainMenu,footer,header{
+                visibility:hidden;}
+                .block-container{
+                    padding-top:1.5rem !important;}
+                h1{
+                    font-family:"Climate Crisis", sans-serif !important;
+                    font-size: 3.5rem !important;
+                    line-height: 1.1 !important;
+                    margin-bottom: 0rem !important;
+                    
+                 }  
+              h2{
+                    font-family:"Climate Crisis", sans-serif !important;
+                    font-size: 2rem !important;
+                    line-height: 0.9 !important;
+                    margin-bottom: 0rem !important;
+                    color:black ;
+                                       
+                } 
+                h3,h4,p{
+                font-family:"Outfit", sans-serif !important;
+                color:black !important;} 
+                
+            
+
+            input[type="text"], input[type="password"] {
+             background-color: black !important;
+             color: white !important;
+             border-radius: 10px !important;
+             padding: 10px !important;
+           }
+
+              /* Placeholder text */
+              div[data-testid="stTextInput"] input::placeholder {
+              color: #bbb !important;
+             }
+
+             /* Remove weird white outer box */
+             div[data-testid="stTextInput"] > div {
+             background: transparent !important;
+              }
+
+
+                button{
+                    border-radius:1.5rem !important;
+                    background:#5B65F2 !important;
+                    color:white !important;
+                    padding:10px 20px !important;
+                    border:none !important;
+                    transition:transform 0.25s ease-in-out !important;}    
+                button[kind="secondary"]{
+                    border-radius:1.5rem !important;
+                    background:#EB459E !important;
+                    color:white !important;
+                    padding:10px 20px !important;
+                    border:none !important;
+                    transition:transform 0.25s ease-in-out !important;}            
+                button[kind="tertiary"]{
+                    border-radius:1.5rem !important;
+                    background:black !important;
+                    color:white !important;
+                    padding:10px 20px !important;
+                    border:none !important;
+                    transition:transform 0.25s ease-in-out !important;}            
+                button:hover{
+                    transform:scale(1.05) ;
+                }
+
+                
+                </style>
+                """, unsafe_allow_html=True)
